b6c57a6d70191c2ede444cbdf668f8766deb9d1b
artur.sudnik@gmail.com <artur.sudnik@gmail.com@5f9c0cbe-a167-ba26-ca4e-03a7b21ac856>
2013-11-02 16:24:12 +0000

    Html cleanup branch

---
+++ branches/2013-11-02_html-cleanup/configureFilePermissions.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+touch jobProcessor_log.html
+chown www-data:www-data jobProcessor_log.html
+
+touch QueueStatus.ini
+chown www-data:www-data QueueStatus.ini
+
+touch temp
+chown www-data:www-data temp
+
+touch templates_c
+chown www-data:www-data templates_c
+
+touch graph/cache
+chown www-data:www-data graph/cache
+
+touch db/wpt_monitor.sqlite
+chown www-data:www-data db/wpt_monitor.sqlite
