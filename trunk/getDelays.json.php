5b91f6f169e4a52feb49291c016d53859ffee1db
artur.sudnik@gmail.com <artur.sudnik@gmail.com@5f9c0cbe-a167-ba26-ca4e-03a7b21ac856>
2013-11-06 10:21:35 +0000

    refactoring

--- trunk/getDelays.json.php
+++ trunk/getDelays.json.php
@@ -1,22 +1,8 @@
 <?php
 require_once('bootstrap.php');
-
+require_once 'jash_functions.inc';
         
-    $conn = Doctrine_Manager::getInstance()->getCurrentConnection();
-    $pdo = $conn->execute("
-        SELECT Location, Agent, count(*) NumberOfJobs, MAX(delayedJobs.delay) AS MaximumDelay
-        FROM (
-            SELECT 
-             j.id, f.label AS Folder, j.label AS JobLabel, j.location AS Agent, l.label Location, FROM_UNIXTIME(lastrun) LastRun, frequency, FROM_UNIXTIME(lastrun + frequency*60) AS NextPlannedRun, SEC_TO_TIME(((UNIX_TIMESTAMP() - lastrun)/60 - frequency)*60) delay
-            FROM WPTJob j
-            JOIN WPTJobFolder f ON f.id = j.wptjobfolderid
-            JOIN WPTLocation l ON j.location = l.location
-            WHERE j.active = 1 AND (lastrun IS NULL OR lastrun + frequency*60 < UNIX_TIMESTAMP())
-        ) delayedJobs
-        GROUP BY Agent, Location
-    ");
-    $pdo->setFetchMode(Doctrine_Core::FETCH_ASSOC);
-    $locations = $pdo->fetchAll();
+    $locations = getDelaysForLocationsAggregated();
     header('Content-type: application/json');
     // print_r(
     echo json_encode(
