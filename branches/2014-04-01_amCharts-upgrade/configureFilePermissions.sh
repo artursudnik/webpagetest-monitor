603ca1f81a68b1fc384e3f067a32059b1d440cfd
artur.sudnik@gmail.com <artur.sudnik@gmail.com@5f9c0cbe-a167-ba26-ca4e-03a7b21ac856>
2014-04-28 18:17:40 +0000

    Merged revision(s) 189-215 from trunk

--- branches/2014-04-01_amCharts-upgrade/configureFilePermissions.sh
+++ branches/2014-04-01_amCharts-upgrade/configureFilePermissions.sh
@@ -7,13 +7,16 @@ touch QueueStatus.ini
 chown www-data:www-data QueueStatus.ini
 
 touch temp
-chown www-data:www-data temp
+chown -R www-data:www-data temp
 
 touch templates_c
 chown www-data:www-data templates_c
+chown www-data:www-data templates_c/*.tpl.php
 
 touch graph/cache
 chown www-data:www-data graph/cache
+chown www-data:www-data graph/cache/*.xml
+chown www-data:www-data graph/cache/.xml
 
 touch db/wpt_monitor.sqlite
 chown www-data:www-data db/wpt_monitor.sqlite
