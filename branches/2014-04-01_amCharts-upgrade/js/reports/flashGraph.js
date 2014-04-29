fee63a16d20c010478fdf2ceaebcec0e4ee8f1fc
artur.sudnik@gmail.com <artur.sudnik@gmail.com@5f9c0cbe-a167-ba26-ca4e-03a7b21ac856>
2014-04-29 09:49:35 +0000

    fixed indentation

--- branches/2014-04-01_amCharts-upgrade/js/reports/flashGraph.js
+++ branches/2014-04-01_amCharts-upgrade/js/reports/flashGraph.js
@@ -15,18 +15,18 @@
         function getChartData(params){
             var d = $.Deferred();
             var ajaxRequestPromise = $.ajax({
-                    url: 'jash/flashGraph.json.php',
-                    data: params,
-                    method: "POST"
-                }).done(function(data){
-                    console.info('recieved data from the server');
-                    if(data.status !== 200) {
-                        console.error(data.status, data.message);
-                        d.reject();
-                    } else {
-                        console.info(data.status, data.message);
-                        d.resolve(data.results);
-                    }
+                url: 'jash/flashGraph.json.php',
+                data: params,
+                method: "POST"
+            }).done(function(data){
+                console.info('recieved data from the server');
+                if(data.status !== 200) {
+                    console.error(data.status, data.message);
+                    d.reject();
+                } else {
+                    console.info(data.status, data.message);
+                    d.resolve(data.results);
+                }
             }); 
             return d.promise();
         }     
