8c474d4d48daf49ef0df5b7471ffcf41e1dc329d
artur.sudnik <artur.sudnik@gmail.com>
2015-03-11 14:33:17 +0000

    small corrections and formatting

--- wptHostStatus.php
+++ wptHostStatus.php
@@ -1,41 +1,40 @@
 <?php
-  require("login/login.php");
-  include_once('monitor.inc');
-  include_once('ec2_functions.inc');
-  include_once('utils.inc');
-  require_once('jash_functions.inc');
-  $testers = getTestersInformation();
-  $locations = getLocationInformation();
-  $runRateInfo = getCurrentRunRateInfo();
-  $cache = true;
+require( "login/login.php" );
+include_once( 'monitor.inc' );
+include_once( 'ec2_functions.inc' );
+include_once( 'utils.inc' );
+require_once( 'jash_functions.inc' );
+$testers     = getTestersInformation();
+$locations   = getLocationInformation();
+$runRateInfo = getCurrentRunRateInfo();
+$cache       = true;
 
-  if ( isset($_REQUEST['cache'])){
+if(isset( $_REQUEST['cache'] )) {
     $cache = $_REQUEST['cache'];
-    if ( $cache == "false" ){
-      $cache = false;
+    if($cache == "false") {
+        $cache = false;
     }
-  }
-  //$ec2TesterStatus = getEC2TesterStatus($cache);
+}
+//$ec2TesterStatus = getEC2TesterStatus($cache);
 
-  foreach($testers as &$tester){
-    foreach($tester['Agents'] as $key=>&$agent){
-      $ec2 = (String) $agent['ec2'];
-      if ( isset($ec2TesterStatus[$ec2])){
-        $agent['ec2Status'] = $ec2TesterStatus[$ec2];
-      }
+foreach ($testers as &$tester) {
+    foreach ($tester['Agents'] as $key => &$agent) {
+        $ec2 = (String)$agent['ec2'];
+        if(isset( $ec2TesterStatus[$ec2] )) {
+            $agent['ec2Status'] = $ec2TesterStatus[$ec2];
+        }
     }
-  }
-  $lastEc2StatusCheck = getEC2TesterStatusLastCheckTime();
+}
+$lastEc2StatusCheck = getEC2TesterStatusLastCheckTime();
 
-  $smarty->assign('lastEc2StatusCheck',$lastEc2StatusCheck);
-  $smarty->assign('locations',$locations);
-  $smarty->assign('testers',$testers);
-  $smarty->assign('runRateInfo',$runRateInfo);
-  $smarty->assign('delayedLocationsAggregated', getDelaysForLocationsAggregated(
+$smarty->assign('lastEc2StatusCheck', $lastEc2StatusCheck);
+$smarty->assign('locations', $locations);
+$smarty->assign('testers', $testers);
+$smarty->assign('runRateInfo', $runRateInfo);
+$smarty->assign('delayedLocationsAggregated', getDelaysForLocationsAggregated(
     array(
-        sort    => 'delay',
-        sortDir => 'desc'
+        'sort'    => 'delay',
+        'sortDir' => 'desc'
     )
-  ));
-  $smarty->display('host/wptHostStatus.tpl');
-?>
+));
+$smarty->display('host/wptHostStatus.tpl');
