26a08f5f67f3e03b24af2554fb5d4fc206eca9c3
Artur Sudnik <artur.sudnik@gmail.com>
2015-09-23 16:12:50 +0200

    corrected bugs

--- configureFilePermissions.sh
+++ configureFilePermissions.sh
@@ -6,17 +6,18 @@ chown www-data:www-data jobProcessor_log.html
 touch QueueStatus.ini
 chown www-data:www-data QueueStatus.ini
 
-touch temp
+mkdir temp
 chown -R www-data:www-data temp
 
-touch templates_c
+mkdir templates_c
 chown www-data:www-data templates_c
 chown www-data:www-data templates_c/*.tpl.php
 
-touch graph/cache
+mkdir graph/cache
 chown www-data:www-data graph/cache
 chown www-data:www-data graph/cache/*.xml
 chown www-data:www-data graph/cache/.xml
+mkdir graph/staticGraphData
 chown www-data:www-data graph/staticGraphData
 
 touch db/wpt_monitor.sqlite
